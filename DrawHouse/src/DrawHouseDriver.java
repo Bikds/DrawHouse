f7ab6ef992fd4822f9a8c458dc5fee391dd2923a
coderator1 <biksharma17@gmail.com>
2017-12-29 19:53:00 -0800

    Version 1

---
+++ DrawHouse/src/DrawHouseDriver.java
@@ -0,0 +1,13 @@
+/*
+ * BikDS
+ * August 23, 2017
+ * This class is the driver class of DrawHouse.java
+ */
+public class DrawHouseDriver{
+		
+  public static void main(String[] args) {
+  	DrawHouse house = new DrawHouse();
+  	
+	house.draw();    
+  }
+}
